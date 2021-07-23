48818b390c169b81e04178906b3d49075da5f779
Yangban2olsida <awfulLCY@gmail.com>
2021-07-23 18:08:19 +0900

    결제

--- kh-final-project/src/main/java/com/kh/finalproject/repository/MemberDaoImpl.java
+++ kh-final-project/src/main/java/com/kh/finalproject/repository/MemberDaoImpl.java
@@ -1,10 +1,13 @@
 package com.kh.finalproject.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.finalproject.entity.MemberDto;
+import com.kh.finalproject.entity.PayDto;
 
 @Repository
 public class MemberDaoImpl implements MemberDao{
@@ -33,8 +36,14 @@ public class MemberDaoImpl implements MemberDao{
 	}
 
 	@Override
-	public int selectHavePoint(int memberHavePoint) {
-		return sqlSession.selectOne("member.selectHavePoint", memberHavePoint);
+	public int selectHavePoint(int memberNo) {
+		return sqlSession.selectOne("member.selectHavePoint", memberNo);
 	}
+
+	@Override
+	public List<PayDto> list(int memberNo) {
+		return sqlSession.selectList("pay.list",memberNo);
+	}
+
 
 }
