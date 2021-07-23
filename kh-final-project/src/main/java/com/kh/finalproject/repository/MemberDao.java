cf6f993aaa3509612eb777470f6f692026cf84d8
Yangban2olsida <awfulLCY@gmail.com>
2021-07-23 18:07:04 +0900

    결제 시도 텥스트

--- kh-final-project/src/main/java/com/kh/finalproject/repository/MemberDao.java
+++ kh-final-project/src/main/java/com/kh/finalproject/repository/MemberDao.java
@@ -1,6 +1,9 @@
 package com.kh.finalproject.repository;
 
+import java.util.List;
+
 import com.kh.finalproject.entity.MemberDto;
+import com.kh.finalproject.entity.PayDto;
 
 public interface MemberDao {
 	boolean checkId(String memberId);
@@ -9,5 +12,5 @@ public interface MemberDao {
 	//회원 번호를 이용해 회원 정보 조회
 	MemberDto getByMemberNo(int memberNo);
 	int selectHavePoint(int memberNo);
-	
+	List<PayDto>list(int memberNo);
 }
