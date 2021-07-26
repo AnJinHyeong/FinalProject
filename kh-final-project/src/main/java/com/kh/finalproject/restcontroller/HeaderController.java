24cbc143aa3e2da839350ec9765d6e1b49039e27
dydqh <dydqh2648@naver.com>
2021-07-26 10:16:01 +0900

    포인트 오류 수정

--- kh-final-project/src/main/java/com/kh/finalproject/restcontroller/HeaderController.java
+++ kh-final-project/src/main/java/com/kh/finalproject/restcontroller/HeaderController.java
@@ -3,22 +3,24 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 //github.com/AnJinHyeong/FinalProject.git
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.finalproject.repository.MemberDao;
 
 @RestController
+@RequestMapping("/header")
 public class HeaderController {
 	
 	@Autowired
 	MemberDao memberDao;
 	
 	@PostMapping("/getMemberHavePoint")
-	public Integer getMemberHavePoint(HttpSession session){
+	public int getMemberHavePoint(HttpSession session){
 		Integer memberNo = (Integer)session.getAttribute("memberNo");
 		if(memberNo == null) {
-			return null;
+			return 0;
 		}
 		return memberDao.getMemberHavePoint(memberNo); 
 	}
