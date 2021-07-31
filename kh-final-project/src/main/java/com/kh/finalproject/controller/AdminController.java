faae63dcfbe18984078f27a7d0f4743a201f1c6a
Yangban2olsida <awfulLCY@gmail.com>
2021-07-31 15:17:23 +0900

    AdminController

--- kh-final-project/src/main/java/com/kh/finalproject/controller/AdminController.java
+++ kh-final-project/src/main/java/com/kh/finalproject/controller/AdminController.java
@@ -1,13 +1,20 @@
 package com.kh.finalproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.finalproject.service.AdminService;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
+	@Autowired
+	AdminService adminService;
+	
 	@GetMapping("")
 	public String adminHome() {
 		return "admin/adminHome";
@@ -18,8 +25,20 @@ public class AdminController {
 		
 		return "admin/adminProjectSelect";
 	}
-
-
+	
+	@GetMapping("/adminMemberSearchAll")
+	public String adminMemberSearchAll(Model model) {
+		
+		model.addAttribute("list",adminService.memberSearchAll());
+		return "admin/adminMemberSearchAll";
+	}
+	
+	@GetMapping("/adminPaySearchAll")
+	public String adminPaySearchAll(Model model) {
+		
+		model.addAttribute("list",adminService.memberSearchAll());
+		return "admin/adminPaySearchAll";
+	}
 
 
 	
