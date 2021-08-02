961830f20ce80c9b84983033c638e7a32500acac
dydqh <dydqh2648@naver.com>
2021-08-02 23:51:20 +0900

    배너 수정, 프로젝트 올리기 수정

--- kh-final-project/src/main/java/com/kh/finalproject/repository/ImageDaoImpl.java
+++ kh-final-project/src/main/java/com/kh/finalproject/repository/ImageDaoImpl.java
@@ -23,7 +23,8 @@ public class ImageDaoImpl implements ImageDao{
 	
 //	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 //	private final File baseDir = new File("/Users/anjinhyeong/sts-workspace/upload/project_image");//저장 경로 //안진형
-	private final File baseDir = new File("D:/upload/finalproject");//저장 경로
+//	private final File baseDir = new File("D:/upload/finalproject");//저장 경로
+	private final File baseDir = new File("D:/upload/kh7d");
 	
 	@Override
 	public ImageDto insertProjectMain(ImageDto imageDto) {
